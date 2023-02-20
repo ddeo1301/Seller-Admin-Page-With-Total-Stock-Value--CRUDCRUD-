e6ffaedf500c50c6029158371232df2a642a61eb
ddeo1301 <divyanshudeo1301@gmail.com>
2023-02-20 23:28:13 +0530

    some more changes

--- dom.js
+++ dom.js
@@ -1,11 +1,9 @@
-//var sum = 0;
 
 window.addEventListener("DOMContentLoaded", () => {
   var sum = 0;
   axios
-    .get("https://crudcrud.com/api/b284a2f3cae3482f822088ce106cc7ca/stockvalue")
+    .get("https://crudcrud.com/api/cb3dd8d7887a44609641752355e0d0c8/stockvalue")
     .then((Response) => {
-        //var sum = 0;
         for (var i = 0; i < Response.data.length; i++) {
             sum = sum + parseInt(Response.data[i].sellPrice);
           }
@@ -32,12 +30,15 @@ function sellerAdminPage(event) {
     nameOfProduct,
   };
 
-  //sum = sum + parseInt(obj.sellPrice);
-
   axios
-    .post("https://crudcrud.com/api/b284a2f3cae3482f822088ce106cc7ca/stockvalue",obj)
+    .post("https://crudcrud.com/api/cb3dd8d7887a44609641752355e0d0c8/stockvalue",obj)
     .then((Response) => {
-      showUserOnScreen(Response.data);
+      var sum = 0;
+      for (var i = 0; i < Response.data.length; i++) {
+        sum = sum + parseInt(Response.data[i].sellPrice);
+      }
+      console.log(sum);
+      showUserOnScreen(Response.data,sum);
       console.log(Response);
     })
     .catch((err) => {
@@ -45,33 +46,16 @@ function sellerAdminPage(event) {
         document.body.innerHTML + `<h4>Something Went Wrong </h4>`;
       console.log(err);
     });
-    location.reload();
 }
 
 function showUserOnScreen(obj,num) {
   const parentItem = document.getElementById("listOfProduct");
-  //const parentItem = document.getElementById("totalStockValue");
   const childItem = document.createElement("li");
-
-  // const childItem1 = document.createElement('h2');
-  // childItem1.textContent = 'Products';
-
-  //const childItem2 = document.createElement('h5');
-//   const item = document.querySelectorAll("#sellingPrice");
-//   console.log(item);
-//   for (var i = 0; i < item.length; i++) {
-//     sum += parseInt(item[i].value);
-//   }
-
-  //childItem2.textContent = 'Total Value Worth Of Product: Rs' + sum;
   const parentItem1 = document.getElementById("total sum");
   parentItem1.textContent = `Total Value Worth Of Product: Rs${num}`;
 
   childItem.textContent = obj.sellPrice + " - " + obj.nameOfProduct;
-
-  // parentItem.appendChild(childItem1);
   parentItem.appendChild(childItem);
-  // parentItem.appendChild(childItem2);
 
   const deleteButton = document.createElement("input");
   deleteButton.type = "button";
@@ -79,22 +63,11 @@ function showUserOnScreen(obj,num) {
   deleteButton.onclick = () => {
     axios
       .delete(
-        `https://crudcrud.com/api/b284a2f3cae3482f822088ce106cc7ca/stockvalue/${obj._id}`
+        `https://crudcrud.com/api/cb3dd8d7887a44609641752355e0d0c8/stockvalue/${obj._id}`
       )
       .then((response) => {
         parentItem.removeChild(childItem);
-        //parentItem.removeChild(childItem1);
-        //parentItem.removeChild(childItem2);
-        //sum -= parseInt(item[item.length-1].value);
-
-        // const item = document.querySelectorAll("#sellingPrice");
-        // console.log(item);
-        // for (var i = 0; i < item.length; i++) {
-        //   sum += parseInt(item[i].value);
-        // }
-        // parentItem1.textContent = "Total Value Worth Of Product: Rs" + sum;
-
-        location.reload();
+        
       })
       .catch((err) => {
         console.log(err);
@@ -103,5 +76,5 @@ function showUserOnScreen(obj,num) {
   };
 
   childItem.appendChild(deleteButton);
-  //parentItem.appendChild(childItem2);
+  parentItem.appendChild(childItem);
 }
